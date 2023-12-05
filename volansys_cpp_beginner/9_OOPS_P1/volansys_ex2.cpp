edf728d7b0df8ceff756c90c055fe4a751027cc6
rohirto <rohirto@gmail.com>
2023-12-05 07:03:07 +0000

    Mega challenge Completed

--- volansys_cpp_beginner/9_OOPS_P1/volansys_ex2.cpp
+++ volansys_cpp_beginner/9_OOPS_P1/volansys_ex2.cpp
@@ -42,77 +42,141 @@
 #include <cstdio>
 #include <bits/stdc++.h>
 #include <numeric>
+#include <cmath>
 
 using namespace std;
 
+
+/**
+ * @brief Debug class to print statements in colored format on terminal
+ * @class Debug
+ * 
+ */
 class Debug{
 public:
+/**
+ * @brief Display a template in red color on terminal
+ * 
+ * @tparam T 
+ * @param str 
+ */
 template <typename T>
 void display_red(T str)
 {
     cout << "\033[1;31m" << str << "\033[0m\n";
 }
+
+/**
+ * @brief Display a template in Green color on terminal
+ * 
+ * @tparam T 
+ * @param str 
+ */
 template <typename T>
 void display_green(T str)
 {
     cout << "\033[1;32m" << str << "\033[0m\n";
 }
+
+/**
+ * @brief Display a template in yellow color on terminal
+ * 
+ * @tparam T 
+ * @param str 
+ */
 template <typename T>
 void display_yellow(T str)
 {
     cout << "\033[1;33m" << str << "\033[0m\n";
 }
+
+/**
+ * @brief Display a template in blue color on terminal
+ * 
+ * @tparam T 
+ * @param str 
+ */
 template <typename T>
 void display_blue(T str)
 {
     cout << "\033[1;34m" << str << "\033[0m\n";
 }
+
+/**
+ * @brief Display the decorator to make output more organized
+ * 
+ */
 void display_decorator()
 {
     cout << "===============================\n";
 }
 
+/**
+ * @brief Construct a new Debug object
+ * 
+ */
 Debug()
 {
 	cout << "Debug Object Created" << endl;
 }
 
+/**
+ * @brief Destroy the Debug object
+ * 
+ */
 ~Debug()
 {
 	cout << "Debug Object Destroyed" << endl;
 }
 
 //Friend Class Declaration
-friend class bank_account;
+friend class bank_account;	/**< Friend class Bank account */
 
 };
 
+/**
+ * @brief bank account class 
+ * @class bank_account
+ */
 class bank_account{
 
+	/**
+	 * @brief struct transactions
+	 * 
+	 */
 	struct transactions
 	{
 		char nature_of_transaction; /**< 'd' Debit, 'c' Credit */
 		double amount;				/**< Amount involved in transaction*/
 		double rem_bal;				/**< Remaining balance */
 	};
-	static int i;
+
 
 private:
 	string depositor_name {}; 		/**< Depositor name*/
 	int account_number;				/**< account number */
 	char type_of_account; 			/**< 'S' Savings, 'C' Current */
+	unsigned int day;				/**< Day of Opening of Acc*/
+	unsigned int month; 			/**< Month of opening of Acc*/
+	unsigned int year;				/**< Year of opening of account */
+	unsigned int no_of_quaters;		/**< No of quaters passed */
 	double balance;					/**< Balance in the account*/
 	//struct transactions t[5];		/**< Last 5 transactions */
 	vector <transactions> t;		/**< Last 5 Transactions */
 
 public:
-bank_account(Debug& d)
+/**
+ * @brief Construct a new bank account object
+ * 
+ * @param d debug object of class Debug -> for printing purpose
+ */
+explicit bank_account(Debug& d)
 {
 	balance = 0;
 	d.display_decorator();
 	d.display_yellow("BANK PROGRAM");
 	d.display_green("1. Enter Account number: ");
-	account_number = get_account_no(d);
+	account_number = get_int(d);
 	d.display_green("2. Enter type of account (S for Savings, C for Cuurent)");
 	type_of_account = get_acc_type(d);
 	d.display_green("Enter Depositor's Name: ");
@@ -120,9 +184,19 @@ bank_account(Debug& d)
 	{
 		d.display_red("Invalid name, try again!");
 	}
+	d.display_green("Enter Account Opening Date: ");
+	while(!get_date(d,day,month,year))
+	{
+		d.display_red("Invalid date, try again!");
+	}
 	
 }
 
+/**
+ * @brief Deposit method - to depo an amount
+ * 
+ * @param d debug object of class Debug -> for printing purpose
+ */
 void deposit(Debug& d)
 {
 	double amount;
@@ -141,6 +215,11 @@ void deposit(Debug& d)
 		d.display_red("Transaction Cancelled!");
 }
 
+/**
+ * @brief Withdraw Method -> to withdraw an amount from account
+ * 
+ * @param d debug object of class Debug -> for printing purpose
+ */
 void withdraw(Debug& d)
 {
 	double amount;
@@ -172,6 +251,12 @@ void withdraw(Debug& d)
 	
 
 }
+
+/**
+ * @brief Method to display account info
+ * 
+ * @param d debug object of class Debug -> for printing purpose
+ */
 void acc_info(Debug& d)
 {
 	d.display_decorator();
@@ -181,10 +266,33 @@ void acc_info(Debug& d)
 	d.display_green(balance);
 }
 
+/**
+ * @brief Method to calculate Simple Interest for given quater
+ * 
+ * @param d 
+ */
 void display_si(Debug& d)
 {
 	// double si = (balance*)
+	// Declare variables
+
+    double principal, rate, interest;
+
+	principal = balance;
+	rate = 4;  //4% Interest Rate
+
+	interest = (principal*rate)/400;
+	d.display_decorator();
+	d.display_yellow("SI for given Quater is: ");
+	d.display_green(interest);
+
 }
+
+/**
+ * @brief Method to display last 5 transactions to user
+ * 
+ * @param d debug object of class Debug -> for printing purpose
+ */
 void mini_statement(Debug& d)
 {
 	d.display_decorator();
@@ -199,12 +307,12 @@ void mini_statement(Debug& d)
 }
 
 private:
-void Display_menu(Debug& d)
-{
-	
-	
-
-}
+/**
+ * @brief Get the acc type method -> savings or cuurent, with error handling
+ * 
+ * @param d debug object of class Debug -> for printing purpose
+ * @return char 'S' or 'C'
+ */
 char get_acc_type(Debug& d)
 {
     char choice;
@@ -222,7 +330,14 @@ char get_acc_type(Debug& d)
     return choice;
 
 }
-int get_account_no(Debug& d)
+
+/**
+ * @brief Get the int method -> gets int variable from cin with error handling
+ * 
+ * @param d debug object of class Debug -> for printing purpose
+ * @return int -> valid int variable
+ */
+int get_int(Debug& d)
 {
 	int a;
 	cin >> a;
@@ -237,6 +352,14 @@ int get_account_no(Debug& d)
 	return a;
 }
 
+/**
+ * @brief Get the dep name Method -> gets a alpha string from cin, no numerals allowes
+ * 
+ * @param d debug object of class Debug -> for printing purpose
+ * @param s string where cin data will be copied to
+ * @return true if valid string received
+ * @return false if invalid string received
+ */
 bool get_dep_name(Debug& d, string& s)
 {
 	getline(cin >> ws,s);
@@ -252,6 +375,14 @@ bool get_dep_name(Debug& d, string& s)
 	return true;
 }
 
+/**
+ * @brief Get the double method, get a double variable from cin, no alpha allowed
+ * 
+ * @param d debug object of class Debug -> for printing purpose
+ * @param num variable where cin data will be copied
+ * @return true if valid double variable
+ * @return false if invalid double variable
+ */
 bool get_double(Debug& d, double& num)
 {
 	string input_msg {};
@@ -280,6 +411,12 @@ bool get_double(Debug& d, double& num)
 	return true;
 }
 
+/**
+ * @brief Update transactions method -> to record transactions when withdrwals and deposits happen
+ * 
+ * @param nature nature of Transcations 'c' -> credit or 'd' -> debit
+ * @param amount involved in transactions
+ */
 void update_transactions(char nature, double amount)
 {
 	if(t.size() > 4)
@@ -296,20 +433,70 @@ void update_transactions(char nature, double amount)
 
 }
 
+/**
+ * @brief Get the date object
+ * 
+ * @param d debug object of class Debug -> for printing purpose
+ * @param dy day
+ * @param m month
+ * @param y year
+ * @return true If valid input from user
+ * @return false If invalid input from user
+ */
+bool get_date(Debug& d, unsigned int& dy,unsigned int& m,unsigned int& y)
+{
+	time_t now = time(0);
+	tm *ltm = localtime(&now);
+
+	d.display_green("Enter Day of Acc opening(DD): ");
+	dy = get_int(d);
+	if((dy < 1) || (dy > 31))
+	{
+		return false;
+	}
+
+	d.display_green("Enter Month of Acc opening(MM): ");
+	m = get_int(d);
+	if((m < 1) || (m > 12))
+	{
+		return false;
+	}
+
+	d.display_green("Enter Year of Acc opening(YYYY): ");
+	y = get_int(d);
+	if(static_cast<int>(y) > (1900 + ltm->tm_year) || static_cast<int>(y) < 1900)
+	{
+		return false;
+	}
+	else if(static_cast<int>(m) > (1 + ltm->tm_mon))
+	{
+		return false;
+	}
+
+	no_of_quaters = ((((1900 + ltm->tm_year) - y)*12)+((1 + ltm->tm_mon) - m))/4;
+
+
+	return true;
+}
+
 
 };
 
-int bank_account::i = 0;
-
+/**
+ * @brief Main function
+ * 
+ * @return int 
+ */
 int main() {
 	Debug debug;
 	bank_account account(debug);
+	char c;
 
-	while(1)
+	do
 	{
-		char c;
+		
 		debug.display_decorator();
-		debug.display_yellow("1. deposit an amount\n2. withdraw an amount\n3. display name and balance\n4. display Interest in given quarter\n5. display mini statement last 5 transections");
+		debug.display_yellow("1. deposit an amount\n2. withdraw an amount\n3. display name and balance\n4. display Interest in given quarter\n5. display mini statement last 5 transections\n (q to quit)");
 		cin >> c;
 
 		switch (c)
@@ -324,15 +511,19 @@ int main() {
 			account.acc_info(debug);
 			break;
 		case '4':
+			account.display_si(debug);
 			break;
 		case '5':
 			account.mini_statement(debug);
+			break;
+		case 'q':
+			debug.display_red("Quitting Program");
 			break;
 		default:
 			debug.display_red("Invalid Input!");
 			break;
 		}
-	}
+	}while(c != 'q');
 
 	
 
